a5d30779ec6a5d7ad7cfc7b6ad11bd385c1e3615
mpiyush15 <pixelsadvertise@gmail.com>
2025-07-12 19:50:43 +0530

    cors eror ypdate irigin to vercel

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: "https://vaibhavgroup.vercel.app", // Allow your frontend domain
   credentials: true
 }));
 

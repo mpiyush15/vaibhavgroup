2fb02330b573fb12371d284c17d7b31892deb9c1
mpiyush15 <pixelsadvertise@gmail.com>
2025-07-12 18:10:35 +0530

    added local host to connection

--- client/utils/connection.js
+++ client/utils/connection.js
@@ -1,6 +1,6 @@
 // client/utils/fetchData.js
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5050/api";
 
 export async function fetchData(endpoint) {
   try {

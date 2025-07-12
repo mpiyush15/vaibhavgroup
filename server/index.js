fdcd861358f5cfad5617dfa62fe98d07cd2a6c13
mpiyush15 <pixelsadvertise@gmail.com>
2025-07-12 17:00:25 +0530

    server index update with port to dynamic

--- server/index.js
+++ server/index.js
@@ -25,4 +25,5 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(5055, () => console.log("Server running on port 5055"));
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

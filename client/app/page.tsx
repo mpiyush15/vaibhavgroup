36060fb76f78b3de3c96fb33e73e52327eda9903
mpiyush15 <pixelsadvertise@gmail.com>
2025-07-12 16:34:36 +0530

    Home page trial chanegs with vaibhav griouo

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Vaibhav Group
           </li>
         </ol>
 

82d919b7a83677e85a33bb0b42af8f1988617eb7
mpiyush15 <pixelsadvertise@gmail.com>
2025-07-12 17:12:27 +0530

    changes done in frontend fetch call to railway

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const [, setData] = useState([]);
 
   useEffect(() => {
-    fetchData("/auth/hello")
+    fetchData(" https://vaibhavgroup-production.up.railway.app/api/auth/hello ")
       .then((res) => setData(res)) 
       .catch((err) => console.error(err));
   }, []);
